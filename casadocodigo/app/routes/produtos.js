c99d84a65ba6c24ddd2679c89cc596a51a82ffc8
Unknown <cyborgmg@gmail.com>
2018-02-11 13:11:01 -0200

    Concluido

---
+++ casadocodigo/app/routes/produtos.js
@@ -0,0 +1,59 @@
+module.exports = function(app) {
+    app.get('/produtos', function(req, res) {
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista(function(err, results) {
+
+            if(err){
+                return next(err);
+            }
+
+            res.format({
+                html: function(){
+                    res.render('produtos/lista',{lista:results});
+                },
+                json: function(){
+                    res.json(results)
+                }
+            });
+        });
+
+        connection.end();
+    });
+
+    app.get('/produtos/form',function(req,res){
+        res.render('produtos/form',{errosValidacao:{}, produto : {}});
+    });
+
+    //verbo HTTP
+    ///produtos/salva
+    //POST default
+    app.post('/produtos',function(req,res){
+
+        var produto = req.body;
+
+        req.assert('titulo','Titulo é obrigatório').notEmpty();;
+        req.assert('preco','Formato inválido').isFloat();
+        var erros = req.validationErrors();
+        if(erros){
+            res.format({
+                html: function(){
+                    res.status(400).render('produtos/form',{errosValidacao:erros,produto:produto});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
+            return;
+        }
+    
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.salva(produto,function(erros,resultados){
+            res.redirect('/produtos');
+        }); 
+
+    });
+}
